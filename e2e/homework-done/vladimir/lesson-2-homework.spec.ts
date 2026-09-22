542bd0e77862cbd1a72e882ae5fe67f24acc8453
kulich51 <kulich51@yandex.ru>
2026-09-22 13:16:07 +0100

    Addressed CR comments

--- e2e/homework-done/vladimir/lesson-2-homework.spec.ts
+++ e2e/homework-done/vladimir/lesson-2-homework.spec.ts
@@ -12,17 +12,20 @@ test.describe('Homework 2: Locators & actions', () => {
     await chatInput.fill('Programmers unite')
     await sendButton.click()
     await searchTab.click()
-    await searchTab.click()
+
     const searchInput = page.getByPlaceholder('Type words from a message…')
     await searchInput.fill('Ducks')
+
     const results = page.locator('li.search-results__item')
     await expect(results).toHaveCount(1)
+
     const ducksResult = results.filter({ hasText: 'Ducks' })
     const texts = ducksResult.locator('.history-exchange__text')
     await expect(texts.first()).toHaveText('Ducks like bread')
     await expect(texts.last()).toHaveText(
       'Ducks think breadcrumbs are cryptocurrency with excellent UX.',
     )
+
     await searchInput.fill('')
     await expect(page.getByText('Enter text to search your local history.')).toBeVisible()
     await expect(results).toHaveCount(0)
