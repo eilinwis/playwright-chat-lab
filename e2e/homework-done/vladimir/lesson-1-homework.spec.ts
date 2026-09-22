d5565432ec4123ff1880cff69f4937d84f599214
kulich51 <kulich51@yandex.ru>
2026-09-22 13:19:18 +0100

    Fixed by comments

--- e2e/homework-done/vladimir/lesson-1-homework.spec.ts
+++ e2e/homework-done/vladimir/lesson-1-homework.spec.ts
@@ -1,21 +1,18 @@
 import { test, expect } from '@playwright/test'
 
-
 test.describe('Homework 1: Getting started', () => {
   test('sending two messages shows their expected replies', async ({page}) => {
-
     const sendButton = page.getByTestId('send-button')
+    const chatInput = page.getByTestId('chat-input')
     await page.goto('/')
-    await expect(page.getByTestId('chat-input')).toBeEnabled({ timeout: 100_000 })
-    await page.getByTestId('chat-input').fill('Queue up the tests')
+    await expect(chatInput).toBeEnabled({ timeout: 15_000 })
+    await chatInput.fill('Queue up the tests')
     await sendButton.click()
 
     await expect(page.getByText('Queue is just Q with four impatient friends standing behind it.')).toBeVisible()
     
-    await page.getByTestId('chat-input').fill('Programmers unite')
+    await chatInput.fill('Programmers unite')
     await sendButton.click()
     await expect(page.getByText('Programmers don’t panic; we just `console.log` our feelings.')).toBeVisible()
-    
   })
-
 })
